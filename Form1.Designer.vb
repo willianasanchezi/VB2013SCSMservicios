ec48928b87c06d23b3559000664da5fc4daaf2c3
Willian A Sanchez I <31877563+willianasanchezi@users.noreply.github.com>
2017-09-20 09:34:10 -0500

    Visual Basic 2013 Servicios System Center Service Manager
    Visual Basic 2013 Servicios System Center Service Manager

---
+++ Form1.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SCSM
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(SCSM))
+        Me.btnservicios = New System.Windows.Forms.Button()
+        Me.btnrenombrar = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnborrar = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnservicios
+        '
+        Me.btnservicios.Location = New System.Drawing.Point(77, 37)
+        Me.btnservicios.Name = "btnservicios"
+        Me.btnservicios.Size = New System.Drawing.Size(150, 23)
+        Me.btnservicios.TabIndex = 0
+        Me.btnservicios.Text = "HealthService"
+        Me.btnservicios.UseVisualStyleBackColor = True
+        '
+        'btnrenombrar
+        '
+        Me.btnrenombrar.Location = New System.Drawing.Point(77, 136)
+        Me.btnrenombrar.Name = "btnrenombrar"
+        Me.btnrenombrar.Size = New System.Drawing.Size(150, 23)
+        Me.btnrenombrar.TabIndex = 1
+        Me.btnrenombrar.Text = "Renombrar Carpeta"
+        Me.btnrenombrar.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(89, 240)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(183, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Desarrollado: Willian Sanchez Ibañez"
+        '
+        'btnborrar
+        '
+        Me.btnborrar.Location = New System.Drawing.Point(77, 165)
+        Me.btnborrar.Name = "btnborrar"
+        Me.btnborrar.Size = New System.Drawing.Size(150, 23)
+        Me.btnborrar.TabIndex = 3
+        Me.btnborrar.Text = "Borrar Carpeta"
+        Me.btnborrar.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(77, 66)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(150, 23)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "OMSDK"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(77, 95)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(150, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "OMCFG"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'SCSM
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(294, 262)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnborrar)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnrenombrar)
+        Me.Controls.Add(Me.btnservicios)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "SCSM"
+        Me.Text = "SCSM Servicios"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnservicios As System.Windows.Forms.Button
+    Friend WithEvents btnrenombrar As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnborrar As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,114 @@
+﻿Imports System
+Imports System.ServiceProcess
+Imports System.Diagnostics
+Imports System.IO
+Imports System.Collections
+
+Public Class SCSM
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnservicios.Click
+        'https://msdn.microsoft.com/es-es/library/system.serviceprocess.servicecontroller(v=vs.110).aspx
+
+        Dim service As ServiceController = New ServiceController("HealthService")
+        Try
+            If ((service.Status.Equals(ServiceControllerStatus.Stopped)) Or
+                        (service.Status.Equals(ServiceControllerStatus.StopPending))) Then
+                service.Start()
+                MsgBox(service.Status)
+            Else
+                service.Stop()
+                MsgBox(service.Status)
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        
+    End Sub
+
+    Private Sub btnrenombrar_Click(sender As Object, e As EventArgs) Handles btnrenombrar.Click
+        'https://msdn.microsoft.com/es-es/library/45we914z(v=vs.100).aspx
+        'Dim directorioold1 As String = "D:\1"
+        'Dim directorioold2 As String = "E:\1"
+        'Dim directorionew As String = "2"
+
+        Dim directorioold1 As String = "D:\Program Files\Microsoft System Center 2012 R2\Service Manager\Health Service State"
+        Dim directorioold2 As String = "E:\Program Files\Microsoft System Center 2012 R2\Service Manager\Health Service State"
+        Dim directorionew As String = "Health Service State1"
+        Try
+            If Directory.Exists(directorioold1) Then
+                My.Computer.FileSystem.RenameDirectory(directorioold1, directorionew)
+                MsgBox("Archivo renombrado" & directorionew)
+            Else
+                If Directory.Exists(directorioold2) Then
+                    My.Computer.FileSystem.RenameDirectory(directorioold2, directorionew)
+                    MsgBox("Archivo renombrado" & directorionew)
+                Else
+                    MsgBox("Rutas no existen: " & vbNewLine & directorioold1 & vbNewLine & directorioold2)
+                End If
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub btnborrar_Click(sender As Object, e As EventArgs) Handles btnborrar.Click
+        'https://msdn.microsoft.com/es-es/library/24t911bf(v=vs.100).aspx
+        Dim eliminar1 As String = "D:\Program Files\Microsoft System Center 2012 R2\Service Manager\Health Service State1"
+        Dim eliminar2 As String = "E:\Program Files\Microsoft System Center 2012 R2\Service Manager\Health Service State1"
+        Try
+            If Directory.Exists(eliminar1) Then
+                My.Computer.FileSystem.DeleteDirectory(eliminar1, FileIO.UIOption.AllDialogs, FileIO.RecycleOption.SendToRecycleBin)
+                MsgBox("Archivo borrado" & eliminar2)
+            Else
+                If Directory.Exists(eliminar2) Then
+                    My.Computer.FileSystem.DeleteDirectory(eliminar2, FileIO.UIOption.AllDialogs, FileIO.RecycleOption.SendToRecycleBin)
+                    MsgBox("Archivo borrado" & eliminar2)
+                Else
+                    MsgBox("Rutas no existen: " & vbNewLine & eliminar1 & vbNewLine & eliminar2)
+                End If
+            End If
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        'https://msdn.microsoft.com/es-es/library/system.serviceprocess.servicecontroller(v=vs.110).aspx
+        Dim service As ServiceController = New ServiceController("OMSDK")
+        Try
+            If ((service.Status.Equals(ServiceControllerStatus.Stopped)) Or
+            (service.Status.Equals(ServiceControllerStatus.StopPending))) Then
+                service.Start()
+                MsgBox(service.Status)
+            Else
+                service.Stop()
+                MsgBox(service.Status)
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        'https://msdn.microsoft.com/es-es/library/system.serviceprocess.servicecontroller(v=vs.110).aspx
+        Dim service As ServiceController = New ServiceController("OMCFG")
+        Try
+            If ((service.Status.Equals(ServiceControllerStatus.Stopped)) Or
+                (service.Status.Equals(ServiceControllerStatus.StopPending))) Then
+                service.Start()
+                MsgBox(service.Status)
+            Else
+                service.Stop()
+                MsgBox(service.Status)
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+
+End Class
